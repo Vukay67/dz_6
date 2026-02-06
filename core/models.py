ab5e0e0cc8769e2d0f626dc6139b7ad2189204d7
Vukay67 <vukay67@gmail.com>
2026-02-06 20:16:16 +0600

    исправлю ошибки

--- core/models.py
+++ core/models.py
@@ -1,11 +1,26 @@
 from django.db import models
 
+COMPANY_CHOICES = [
+    ['Airbus', 'Airbus'],
+    ['Boeing', 'Boeing'],
+    ['Embraer', 'Embraer'],
+    ['Bombardier', 'Bombardier'],
+    ['ATR', 'ATR'],
+    ['Сухой', 'Сухой'],
+    ['Иркут', 'Иркут'],
+    ['COMAC', 'COMAC'],
+    ['Gulfstream', 'Gulfstream'],
+    ['Dassault', 'Dassault'],
+    ['Cessna', 'Cessna']
+]
+
 class Aircraft(models.Model):
     image = models.ImageField(upload_to="menus/")
-    name = models.CharField(max_length=70)
+    model = models.CharField(max_length=70)
+    companys = models.CharField(choices=COMPANY_CHOICES, max_length=20)
     year_manufacture = models.PositiveIntegerField()
     raid = models.PositiveIntegerField()
     current_rate = models.PositiveIntegerField()
 
     def __str__(self):
-        return f"{self.name} (${self.current_rate}) (Налёт: {self.raid})"
+        return f"{self.companys}{self.model} (${self.current_rate}) (Налёт: {self.raid})"
--- core/views.py
+++ core/views.py
@@ -4,6 +4,44 @@ from .models import *
 
 def main_page(request):
     aircraft = Aircraft.objects.all()
+
+    n_model = request.GET.get("name")
+    n_company = request.GET.get("company")
+    price = request.GET.get("price")
+
+    if n_model is not None and len(n_model) > 0:
+        aircraft = aircraft.filter(model__icontains =n_model)
+
+    if n_company is not None:
+        if n_company == "Airbus":
+            aircraft = aircraft.filter(companys__icontains =n_company)
+        elif n_company == "Boeing":
+            aircraft = aircraft.filter(companys__icontains =n_company)
+        elif n_company == "Embraer":
+            aircraft = aircraft.filter(companys__icontains =n_company)
+        elif n_company == "Bombardier":
+            aircraft = aircraft.filter(companys__icontains =n_company)
+        elif n_company == "ATR":
+            aircraft = aircraft.filter(companys__icontains =n_company)
+        elif n_company == "Сухой":
+            aircraft = aircraft.filter(companys__icontains =n_company)
+        elif n_company == "Иркут":
+            aircraft = aircraft.filter(companys__icontains =n_company)
+        elif n_company == "COMAC":
+            aircraft = aircraft.filter(companys__icontains =n_company)
+        elif n_company == "Gulfstream":
+            aircraft = aircraft.filter(companys__icontains =n_company)
+        elif n_company == "Dassault":
+            aircraft = aircraft.filter(companys__icontains =n_company)
+        elif n_company == "Cessna":
+            aircraft = aircraft.filter(companys__icontains =n_company)
+
+    if price is not None:
+        if price == "price_asc":
+            aircraft = aircraft.order_by('current_rate')
+        elif price == "price_desc":
+            aircraft = aircraft.order_by('-current_rate')
+
     context = {
         "aircraft": aircraft
     }
